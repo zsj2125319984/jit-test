46a529b898d9786d4f659cd304bb0bf3b29d2069
Sicecream <zhengsj2125319984@outlook.com>
2025-06-08 23:39:45 +0800

    test 2.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git 2.0");
     }
 }
